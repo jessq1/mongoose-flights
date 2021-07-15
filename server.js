b82bf0a318cc28002bac82ca3361d39fbbd49924
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-15 13:55:12 -0400

    set-up

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import('./config/database.js')
+
 
 import { router as indexRouter } from './routes/index.js'
 import { router as flightsRouter } from './routes/flights.js'
